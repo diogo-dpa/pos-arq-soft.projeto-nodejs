e3e07fc9394ffc1e4813563c7aa478895718e35e
Diogo Almazan <diogodpa@outlook.com>
2023-09-01 22:05:46 -0300

    Removed prefix api/v1

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan("common"));
 app.use(helmet());
 
 const apiV1Router = require("./routes/apiV1Router");
-app.use("/api/v1", apiV1Router);
+app.use("/", apiV1Router);
 
 app.listen(process.env.PORT || 3000, function () {
 	console.log("Servidor rodando na porta 3000");
